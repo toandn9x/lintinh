752e41946ad1bef3e63b2fc37a4315fbd6151eee
Toan <82641083+toandn9x@users.noreply.github.com>
2021-12-07 11:57:51 +0700

    1st
    1st

---
+++ countDownTet2021/js/main.js
@@ -0,0 +1,17 @@
+﻿    document.addEventListener("DOMContentLoaded", function() {
+        let targetDate = new Date("'01/01/2021 00:00:00 GMT+0700 (SE Asia Standard Time)");
+        let onStart = () => {
+            document.querySelectorAll('.countdown-item').forEach(item => item.classList.add('show'))
+        }
+        let onTick = ({ days, hours, minutes, seconds }) => {
+            document.querySelector('.countdown-item.days').innerHTML = days;
+            document.querySelector('.countdown-item.hours').innerHTML = hours;
+            document.querySelector('.countdown-item.minutes').innerHTML = minutes;
+            document.querySelector('.countdown-item.seconds').innerHTML = seconds;
+        };
+        let options = new LsCountdownOptions({ targetDate, onStart, onTick });
+        let countdown = new LsCountdown(options);
+
+        countdown.start();
+        window.mycountdown = countdown;
+    });
